8a2987bcca79c10ae8c4314166e85b81662897fc
marikadiblasio <marbla.albram@gmail.com>
2023-04-02 16:06:29 +0200

    add static structure

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ createApp(
                         {
                             id:1,
                             name: 'Michele',
-                            avatar: './img/avatar_1.png',
+                            avatar: './img/avatar_1.jpg',
                             visible: true,
                             messages: [
                                 {
@@ -31,7 +31,7 @@ createApp(
                         {
                             id:2,
                             name: 'Fabio',
-                            avatar: './img/avatar_2.png',
+                            avatar: './img/avatar_2.jpg',
                             visible: true,
                             messages: [
                                 {
@@ -54,7 +54,7 @@ createApp(
                         {
                             id:3,
                             name: 'Samuele',
-                            avatar: './img/avatar_3.png',
+                            avatar: './img/avatar_3.jpg',
                             visible: true,
                             messages: [
                                 {
@@ -77,7 +77,7 @@ createApp(
                         {
                             id:4,
                             name: 'Alessandro B.',
-                            avatar: './img/avatar_4.png',
+                            avatar: './img/avatar_4.jpg',
                             visible: true,
                             messages: [
                                 {
@@ -95,7 +95,7 @@ createApp(
                         {
                             id:5,
                             name: 'Alessandro L.',
-                            avatar: './img/avatar_5.png',
+                            avatar: './img/avatar_5.jpg',
                             visible: true,
                             messages: [
                                 {
@@ -113,7 +113,7 @@ createApp(
                         {
                             id:6,
                             name: 'Claudia',
-                            avatar: './img/avatar_5.png',
+                            avatar: './img/avatar_5.jpg',
                             visible: true,
                             messages: [
                                 {
@@ -136,7 +136,7 @@ createApp(
                         {
                             id:7,
                             name: 'Federico',
-                            avatar: './img/avatar_7.png',
+                            avatar: './img/avatar_7.jpg',
                             visible: true,
                             messages: [
                                 {
@@ -154,7 +154,7 @@ createApp(
                         {
                             id:8,
                             name: 'Davide',
-                            avatar: './img/avatar_8.png',
+                            avatar: './img/avatar_8.jpg',
                             visible: true,
                             messages: [
                                 {
